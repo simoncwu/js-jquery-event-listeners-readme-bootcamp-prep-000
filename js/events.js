d22ccf77253ee85984a55dc3404fb9659a6c40a2
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-16 16:00:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $(this).addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(e) {
+    if (e.which == 71)
+      alert("G was pressed!");
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
